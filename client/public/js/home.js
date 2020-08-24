ac7e61ab05c62435a992eb0ba551462c742c5fcb
mohammad7958 <57726445+mohammad7958@users.noreply.github.com>
2020-08-25 00:18:29 +0300

    using the bootstrap collections recreate the home page and the upper bar and the navbar of the website and add it to all html files and write the code of the css for them and create home.js file and add the slider pictures and fix the css file not found problem and add some upgrades for the server files

---
+++ client/public/js/home.js
@@ -0,0 +1,8 @@
+$(() => {
+	const st = 'use strict';
+
+	const winH = $(window).height();
+	const upperH = $('.upper-bar').innerHeight();
+	const navH = $('.navbar').innerHeight();
+	$('.slider .carousel-item').height(winH - (upperH + navH));
+});
--- server.js
+++ server.js
@@ -10,13 +10,10 @@ const { parseUser, anonymouse } = require('./middlewares/auth');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-	console.log(`Node server is running on port ${PORT}...`);
-});
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, 'client', 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.urlencoded()); // Parse URL-encoded bodies
 app.use(parseUser);
 app.use(authRouter);
@@ -25,12 +22,16 @@ app.get('/', (req, res) => {
 	fs.createReadStream('./client/home.html').pipe(res);
 });
 
-app.get('/main', (req, res) => {
-	fs.createReadStream('./client/main.html').pipe(res);
+app.get('/home', (req, res) => {
+	fs.createReadStream('./client/home.html').pipe(res);
 });
 app.get('/login', anonymouse, (req, res) => {
 	fs.createReadStream('./client/login.html').pipe(res);
 });
 app.get('/register', anonymouse, (req, res) => {
 	fs.createReadStream('./client/reg.html').pipe(res);
+});
+
+app.listen(PORT, () => {
+	console.log(`Node server is running on port ${PORT}...`);
 });
