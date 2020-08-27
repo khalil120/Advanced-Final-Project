a000ce082cfa8ac722c442728ab5e476625ba76f
mohammad7958 <57726445+mohammad7958@users.noreply.github.com>
2020-08-27 18:45:10 +0300

    fix about us page problem

--- server.js
+++ server.js
@@ -19,27 +19,25 @@ app.use(parseUser);
 app.use(authRouter);
 
 app.get('/', (req, res) => {
-    fs.createReadStream('./client/home.html').pipe(res);
+	fs.createReadStream('./client/home.html').pipe(res);
 });
 
 app.get('/home', (req, res) => {
-    fs.createReadStream('./client/home.html').pipe(res);
+	fs.createReadStream('./client/home.html').pipe(res);
 });
 app.get('/login', anonymouse, (req, res) => {
-    fs.createReadStream('./client/login.html').pipe(res);
+	fs.createReadStream('./client/login.html').pipe(res);
 });
 app.get('/register', anonymouse, (req, res) => {
-    fs.createReadStream('./client/reg.html').pipe(res);
+	fs.createReadStream('./client/reg.html').pipe(res);
 });
-
 app.get('/contact-us', anonymouse, (req, res) => {
-    fs.createReadStream('./client/contact-us.html').pipe(res);
+	fs.createReadStream('./client/contact-us.html').pipe(res);
 });
-
 app.get('/post-car', anonymouse, (req, res) => {
-    fs.createReadStream('./client/post.html').pipe(res);
+	fs.createReadStream('./client/post.html').pipe(res);
 });
 
 app.listen(PORT, () => {
-    console.log(`Node server is running on port ${PORT}...`);
+	console.log(`Node server is running on port ${PORT}...`);
 });
