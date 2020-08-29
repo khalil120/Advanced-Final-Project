c554fa106d3ecdf2fbc185e533efc0292c9fa1ee
noor <noor.bakrieh@gmail.com>
2020-08-29 21:56:21 +0300

    search car update

--- client/public/js/search-car.js
+++ client/public/js/search-car.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+function show() {
 	$('#lease_cars_div').append($('<div class="cars_container">\n'
     + '                <img class="cars_image" src="public/images/test1.png" height="150" width="100%"/>\n'
     + '                <p class="cars_price" >189₪ per day </p>\n'
@@ -39,4 +39,10 @@ $(document).ready(() => {
     + '                <p class="available" >Available!</p>\n'
     + '                <button class="cars_order" >Show deal</button>\n'
     + '            </div>'));
-});
+}
+function getSelect() {
+	document.getElementById('infoC').style.height = '400px';
+	document.getElementById('infoC').style.background = '#0a5b79';
+
+	document.getElementById('inner').style.display = 'contents';
+}
