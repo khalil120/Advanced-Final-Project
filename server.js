1147f528c1912f915d4def8b2f2ae0579fa7a313
saher90 <sahernaoum@gmail.com>
2020-08-30 16:53:25 +0300

    added middleware for uploading image to dataBase and downloading the image from data base

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
+const methodOverride = require('method-override');
 const authRouter = require('./routes/auth');
+const routerupload = require('./routes/upload');
 
 const { parseUser, anonymouse } = require('./middlewares/auth');
 
@@ -17,6 +19,8 @@ app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.urlencoded()); // Parse URL-encoded bodies
 app.use(parseUser);
 app.use(authRouter);
+app.use(routerupload);
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
 	fs.createReadStream('./client/home.html').pipe(res);
@@ -31,7 +35,9 @@ app.get('/login', anonymouse, (req, res) => {
 app.get('/register', anonymouse, (req, res) => {
 	fs.createReadStream('./client/reg.html').pipe(res);
 });
-
+app.get('/upload', (req, res) => {
+	fs.createReadStream('./client/uploadimg.html').pipe(res);
+});
 app.listen(PORT, () => {
 	console.log(`Node server is running on port ${PORT}...`);
 });
