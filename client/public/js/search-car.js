216ce180136ca6757e59bce129aa2738ae5303ef
saher90 <sahernaoum@gmail.com>
2020-09-07 16:24:41 +0300

    small fix after last puch

--- client/public/js/search-car.js
+++ client/public/js/search-car.js
@@ -13,11 +13,12 @@ $(document).ready(() => {
 			console.log(res);
 
 			$.each(res, (index, item) => {
+				console.log(item);
 				$(divName).append(`<div class="cars_container" id="${item.filename}">
                <img class="cars_image" src="public/img/${item.filename}" height="150" width="100%"/>
                 <p class="cars_price" >${item.price}</p>
-               <p class="cars_name" >${item.model}</p>
-                <p class="cars_desc" >${item.seats} Adults, ${item.bags} bags</p>
+               <p class="cars_name" >${item.carModel}</p>
+                <p class="cars_desc" >${item.seats} Adults, ${item.airBags} bags</p>
                <p class="available" >Available!</p>
                 <button class="cars_order" >Show deal</button>
                 </div>`);
