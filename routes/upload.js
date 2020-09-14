949a24fb349e357ceaeb7f23c5e62c0ae05f4daa
khalil120 <khaleel.kblawe@gmail.com>
2020-09-14 11:41:00 +0300

    small fixes after last PQ

--- routes/upload.js
+++ routes/upload.js
@@ -1,3 +1,6 @@
+/* eslint-disable eol-last */
+/* eslint-disable indent */
+/* eslint-disable no-unused-expressions */
 const express = require('express');
 const crypto = require('crypto');
 const multer = require('multer');
@@ -20,203 +23,202 @@ const dataurl = `${url}/heroku_342hvvg9`;
 // make sure the db instance is open before passing into `Grid`
 
 MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
-	assert.equal(null, err);
-	const db = client.db(dbName);
-	const bucket = new mongodb.GridFSBucket(db, {
-		bucketName: 'imgRent',
-	});
+    assert.equal(null, err);
+    const db = client.db(dbName);
+    const bucket = new mongodb.GridFSBucket(db, {
+        bucketName: 'imgRent',
+    });
 
-	const storage = new GridFsStorage({
-		url, // mongodb Connection URL
-		file: (req, file) => new Promise((resolve, reject) => {
-			crypto.randomBytes(16, (err, buf) => {
-				if (err) {
-					return reject(err);
-				}
-				const filename = buf.toString('hex') + path.extname(file.originalname);
-				// check type of upload if rent or sale
-				const fileInfo = {
-					filename,
-					bucketName: 'imgRent',
-				};
-				resolve(fileInfo);
-			});
-		}),
-	});
-	const upload = multer({ storage });
+    const storage = new GridFsStorage({
+        url, // mongodb Connection URL
+        file: (req, file) => new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buf) => {
+                if (err) {
+                    return reject(err);
+                }
+                const filename = buf.toString('hex') + path.extname(file.originalname);
+                // check type of upload if rent or sale
+                const fileInfo = {
+                    filename,
+                    bucketName: 'imgRent',
+                };
+                resolve(fileInfo);
+            });
+        }),
 
-	routerUpload.post('/upload-sale', upload.single('img'), (req, res) => {
-		const collection = db.collection('carSale');
+    });
+    const upload = multer({ storage });
 
-		const {
-			milege,
-			engineType,
-			gearBox,
-			carColor,
-			roadEntry,
-			price,
-			carModel,
-			airBags,
-			seats,
-		} = req.body;
-		const { username } = req.user;
-		const data = {
-			username,
-			milege,
-			engineType,
-			gearBox,
-			carColor,
-			roadEntry,
-			price,
-			carModel,
-			airBags,
-			seats,
-			filename: req.file.filename,
+    routerUpload.post('/upload-sale', upload.single('img'), (req, res) => {
+        const collection = db.collection('carSale');
 
-		};
-		collection.insertOne(data);
-		res.sendStatus(200);
-	});
-	routerUpload.post('/upload-rent', upload.single('img'), (req, res) => {
-		const collection = db.collection('carRent');
-		const {
-			engineType,
-			gearBox,
-			carColor,
-			roadEntry,
-			priceDay,
-			toDate,
-			fromDate,
-			carModel,
-			airBags,
-			seats,
-		} = req.body;
-		const { username } = req.user;
-		const data = {
-			username,
-			priceDay,
-			engineType,
-			gearBox,
-			carColor,
-			roadEntry,
-			toDate,
-			fromDate,
-			carModel,
-			airBags,
-			seats,
-			filename: req.file.filename,
-		};
-		collection.insertOne(data);
-		res.sendStatus(200);
-	});
-	routerUpload.post('/show-buy', (req, res) => {
-		// add file name in get request
-		const { carType } = req.body;
+        const {
+            milege,
+            engineType,
+            gearBox,
+            carColor,
+            roadEntry,
+            price,
+            carModel,
+            airBags,
+            seats,
+        } = req.body;
+        const { username } = req.user;
+        const data = {
+            username,
+            milege,
+            engineType,
+            gearBox,
+            carColor,
+            roadEntry,
+            price,
+            carModel,
+            airBags,
+            seats,
+            filename: req.file.filename,
 
-		const collection = db.collection('carSale');
-		if (carType.length > 0) {
-			collection.find({ carModel: carType }).toArray((err, docs) => {
-				assert.equal(err, null);
-				const array = [];
-				const imageNmae = [];
-				docs.forEach((element) => {
-					array.push(element);
-					imageNmae.push(element.filename);
-				});
-				imageNmae.forEach((fileName) => {
-					bucket.openDownloadStreamByName(fileName).pipe(
-						fs.createWriteStream(`./client/public/img/${fileName}`),
-					).on('error',
-						(error) => {
-							console.log('Error:-', error);
-							res.sendStatus(404);
-						}).on('finish', () => {
-						console.log(`${fileName} download complete!`);
-					});
-				});
+        };
+        collection.insertOne(data);
+        res.sendStatus(200);
+    });
+    routerUpload.post('/upload-rent', upload.single('img'), (req, res) => {
+        const collection = db.collection('carRent');
+        const {
+            engineType,
+            gearBox,
+            carColor,
+            roadEntry,
+            priceDay,
+            toDate,
+            fromDate,
+            carModel,
+            airBags,
+            seats,
+        } = req.body;
+        const { username } = req.user;
+        const data = {
+            username,
+            priceDay,
+            engineType,
+            gearBox,
+            carColor,
+            roadEntry,
+            toDate,
+            fromDate,
+            carModel,
+            airBags,
+            seats,
+            filename: req.file.filename,
+        };
+        collection.insertOne(data);
+        res.sendStatus(200);
+    });
+    routerUpload.post('/show-buy', (req, res) => {
+        // add file name in get request
+        const { carType } = req.body;
 
-				res.status(200).send(array);
-			});
-		} else {
-			collection.find({}).toArray((err, docs) => {
-				assert.equal(err, null);
+        const collection = db.collection('carSale');
+        if (carType.length > 0) {
+            collection.find({ carModel: carType }).toArray((err, docs) => {
+                assert.equal(err, null);
+                const array = [];
+                const imageNmae = [];
+                docs.forEach((element) => {
+                    array.push(element);
+                    imageNmae.push(element.filename);
+                });
+                imageNmae.forEach((fileName) => {
+                    bucket.openDownloadStreamByName(fileName).pipe(
+                        fs.createWriteStream(`./client/public/img/${fileName}`),
+                    ).on('error',
+                        (error) => {
+                            console.log('Error:-', error);
+                            res.sendStatus(404);
+                        }).on('finish', () => {
+                        console.log(`${fileName} download complete!`);
+                    });
+                });
 
-				console.log(err);
-				const array = [];
-				const imageNmae = [];
-				docs.forEach((element) => {
-					array.push(element);
-					imageNmae.push(element.filename);
-				});
-				imageNmae.forEach((fileName) => {
-					bucket.openDownloadStreamByName(fileName).pipe(
-						fs.createWriteStream(`./client/public/img/${fileName}`),
-					).on('error',
-						(error) => {
-							console.log('Error:-', error);
-						}).on('finish', () => {
-						console.log(`${fileName} download complete!`);
-					});
-				});
-				res.status(200).send(array);
-			});
-		}
-	});
-	routerUpload.post('/show-rent', (req, res) => {
-		// add file name in get request
-		// add search via date and price
+                res.status(200).send(array);
+            });
+        } else {
+            collection.find({}).toArray((err, docs) => {
+                assert.equal(err, null);
+                const array = [];
+                const imageNmae = [];
+                docs.forEach((element) => {
+                    array.push(element);
+                    imageNmae.push(element.filename);
+                });
+                imageNmae.forEach((fileName) => {
+                    bucket.openDownloadStreamByName(fileName).pipe(
+                        fs.createWriteStream(`./client/public/img/${fileName}`),
+                    ).on('error',
+                        (error) => {
+                            console.log('Error:-', error);
+                        }).on('finish', () => {
+                        console.log(`${fileName} download complete!`);
+                    });
+                });
+                res.status(200).send(array);
+            });
+        }
+    });
+    routerUpload.post('/show-rent', (req, res) => {
+        // add file name in get request
+        // add search via date and price
 
-		const { toDate, fromDate } = req.body;
-		const { carType } = req.body;
-		const collection = db.collection('carRent');
-		if (carType.length > 0) {
-			collection.find({ carModel: carType }).toArray((err, docs) => {
-				assert.equal(err, null);
-				imageNmae.push(element.filename);
-				const array = [];
-				const imageNmae = [];
-				docs.forEach((element) => {
-					array.push(element);
-					imageNmae.push(element.filename);
-				});
-				imageNmae.forEach((fileName) => {
-					bucket.openDownloadStreamByName(fileName).pipe(
-						fs.createWriteStream(`/client/public/img/${fileName}`),
-					).on('error',
-						(error) => {
-							console.log('Error:-', error);
-							res.sendStatus(404);
-						}).on('finish', () => {
-						console.log(`${fileName} download complete!`);
-					});
-				});
+        const { toDate, fromDate } = req.body;
+        const { carType } = req.body;
+        const collection = db.collection('carRent');
 
-				res.status(200).send(array);
-			});
-		} else { // show all available cars for rent
-			console.log(`cartType not  inserted.... ${url}`); /// ///////////////////////////////////////////////
-			// the problem is docs is empty !!!
-			collection.find({}).toArray((err, docs) => {
-				assert.equal(err, null);
-				const array = [];
-				const imageNmae = [];
-				docs.forEach((element) => {
-					array.push(element);
-					imageNmae.push(element.filename);
-				});
-				imageNmae.forEach((fileName) => {
-					bucket.openDownloadStreamByName(fileName).pipe(
-						fs.createWriteStream(`/client/public/img/${fileName}`),
-					).on('error',
-						(error) => {
-							console.log('Error:-', error);
-						}).on('finish', () => {
-						console.log(`${fileName} download complete!`);
-					});
-					res.status(200).send(array);
-				});
-			});
-		}
-	});
+        if (carType.length > 0) {
+            collection.find({ carModel: carType }).toArray((err, docs) => {
+                assert.equal(err, null);
+                imageNmae.push(element.filename);
+                const array = [];
+                const imageNmae = [];
+                docs.forEach((element) => {
+                    array.push(element);
+                    imageNmae.push(element.filename);
+                });
+                imageNmae.forEach((fileName) => {
+                    bucket.openDownloadStreamByName(fileName).pipe(
+                        fs.createWriteStream(`./client/public/img/${fileName}`),
+                    ).on('error',
+                        (error) => {
+                            console.log('Error:-', error);
+                            res.sendStatus(404);
+                        }).on('finish', () => {
+                        console.log(`${fileName} download complete!`);
+                    });
+                });
+
+                res.status(200).send(array);
+            });
+        } else { // show all available cars for rent
+            collection.find({}).toArray((err, docs) => {
+                assert.equal(err, null);
+                const array = [];
+                const imageNmae = [];
+                docs.forEach((element) => {
+                    array.push(element);
+                    imageNmae.push(element.filename);
+                });
+                imageNmae.forEach((fileName) => {
+                    bucket.openDownloadStreamByName(fileName).pipe(
+                        fs.createWriteStream(`./client/public/img/${fileName}`),
+                    ).on('error',
+                        (error) => {
+                            console.log('Error:-', error);
+                        }).on('finish', () => {
+                        console.log(`${fileName} download complete!`);
+                    });
+                    res.send(array);
+                });
+            });
+        }
+    });
 });
+
 module.exports = routerUpload;
