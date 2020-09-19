5a6898016c179f6df27b57acd24973f4ecd39f73
khalil120 <khaleel.kblawe@gmail.com>
2020-09-19 17:53:01 +0300

    small fix

--- routes/upload.js
+++ routes/upload.js
@@ -217,7 +217,7 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
 						}).on('finish', () => {
 						console.log(`${fileName} download complete!`);
 					});
-					res.send(array);
+					res.status(200).send(array);
 				});
 			});
 		}
