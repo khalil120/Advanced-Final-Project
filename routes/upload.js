663e4d0f49afb90ec6f2c4acc4c591a0701e29b9
khalil120 <khaleel.kblawe@gmail.com>
2020-09-22 20:08:39 +0300

    delete last update

--- routes/upload.js
+++ routes/upload.js
@@ -118,7 +118,6 @@ MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
 	});
 
 	routerUpload.post('/insert-order', (req, res) => {
-		console.log(req);
 		console.log(req.user.username);
 		const collection = db.collection('orders');
 		const {
