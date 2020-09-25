2f5fe9e534aba319a9da51d00d5db763780e5162
khalil120 <khaleel.kblawe@gmail.com>
2020-09-25 18:25:36 +0300

    added profile page and routes updated

--- server.js
+++ server.js
@@ -44,6 +44,10 @@ app.get('/contact-us', (req, res) => {
 app.get('/search-car', authorized, (req, res) => {
 	fs.createReadStream('./client/search-car.html').pipe(res);
 });
+
+app.get('/profile', authorized, (req, res) => {
+	fs.createReadStream('./client/profile.html').pipe(res);
+});
 app.listen(PORT, () => {
 	console.log(`Node server is running on port ${PORT}...`);
 });
